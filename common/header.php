fc96d4ed81a02d4d3e54d6fa9d7dae2ee10686e4
greentomatoma <greentomatoma@gmail.com>
2021-09-04 10:32:37 +0900

    ヘッダー情報の出力関数_作成

---
+++ common/header.php
@@ -0,0 +1,20 @@
+<?php 
+  /**
+   * タイトルを指定してヘッダーを作成
+   * @param $title
+   * @return string
+   */
+  function getHeader($title) {
+    return <<< EOF
+    <head>
+      <meta charset="utf-8" />
+      <title>Memo_app | {$title}</title>
+      <link rel="stylesheet" type="text/css" href="../public/css/main.css" />
+      <script defer src="../public/js/all.js"></script>
+    </head>
+  EOF;
+  }
+
+
+
+?>
