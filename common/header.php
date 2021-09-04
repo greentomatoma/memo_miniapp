dbb1ebf2093db9332ba72c1f1772eb08ac9af529
greentomatoma <greentomatoma@gmail.com>
2021-09-04 11:39:30 +0900

    アイコン読み込み

--- common/header.php
+++ common/header.php
@@ -11,6 +11,7 @@
       <title>Memo_app | {$title}</title>
       <link rel="stylesheet" type="text/css" href="../public/css/main.css" />
       <link rel='stylesheet' href='https://unpkg.com/ress/dist/ress.min.css'>
+      <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
       <script defer src="../public/js/all.js"></script>
     </head>
   EOF;
